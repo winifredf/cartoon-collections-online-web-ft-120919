b850c58e7a140c90dafbeb8d0e3d3317c9c76f40
winifredf <winifred.frierson@gmail.com>
2022-06-07 18:12:36 -0400

    commit tree be75125754c8438e9d39254e422d73e579d37e38

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,3 +22,16 @@ def find_the_cheese(array)# code an argument here
       
     end
 end
+# def roll_call_dwarves(array)
+#   index = 0
+#   array.each_with_index{|name, index| puts "#{index + 1}. #{name}"}
+# end
+
+# def summon_captain_planet(array)
+#   array.each do |planeteer_calls|
+#     planeteer_calls = []
+#     planeteer_calls << "#{planeteer_calls.upcase}!"
+#   end
+
+#   planeteer_calls
+# end
